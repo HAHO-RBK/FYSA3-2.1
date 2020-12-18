af163bc791adda76641c0f73b8c44a51b8a21d14
hibaelechi@gmail.com <hibaelechi@gmail.com>
2020-12-18 17:06:24 +0100

    nav

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,22 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin/admin.component';
 import { WorkerComponent } from './worker/worker.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './user/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './user/home/home.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { RoutingModule } from './user/routing.module';
 @NgModule({
-  declarations: [AppComponent, UserComponent, AdminComponent, WorkerComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, UserComponent, AdminComponent, WorkerComponent, NavComponent, HomeComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, RoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
